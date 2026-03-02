10083def3e0642481d0b4ee247ce859234af69d9
9t2jbfs99t-creator <9t2jbfs99t@privaterelay.appleid.com>
2026-03-02 07:51:20 +0100

    Update models.js

--- api/models.js
+++ api/models.js
@@ -1,22 +1,15 @@
 export default async function handler(req, res) {
-  const { make, year } = req.query;
-
-  if (!make) {
-    return res.status(400).json([]);
-  }
+  const make = req.query.make;
+  if (!make) return res.status(200).json([]);
 
   try {
-    const url = `https://carapi.app/api/models?make=${encodeURIComponent(
-      make
-    )}&year=${year || 2020}`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const models = data.data.map(m => m.name);
-
-    res.status(200).json(models);
-  } catch (err) {
-    res.status(500).json([]);
+    const r = await fetch(
+      `https://api.api-ninjas.com/v1/carmodels?make=${make}`,
+      { headers: { "X-Api-Key": process.env.CAR_API_KEY } }
+    );
+    const data = await r.json();
+    res.status(200).json(data || []);
+  } catch {
+    res.status(200).json([]);
   }
 }
